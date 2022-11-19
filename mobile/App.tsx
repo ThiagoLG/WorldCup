11c05aaf814c90cded0c2f23ab97086282c012c6
Thiago Lourençon Ghebra <thiago.lg53@gmail.com>
2022-11-19 20:31:28 -0300

    Criação do projeto mobile + configuração base da página principal, temas, componente de loading e base do componente de login

---
+++ mobile/App.tsx
@@ -0,0 +1,31 @@
+/*- Componentes de plugins e bibliotecas -*/
+import { NativeBaseProvider, StatusBar } from "native-base";
+import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto'
+
+/*- Componentes, configurações e utilitários internos -*/
+import { Loading } from './src/components/loading'
+import { SignIn } from './src/screens/SignIn'
+import { THEME } from './src/styles/theme'
+
+/**
+ * Componente principal do projeto, referente a página inicial
+ * @returns Página inicial da aplicação
+ */
+export default function App() {
+
+  /*- Controla o carregamento de fontes -*/
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold });
+
+  return (
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle={"light-content"}
+        backgroundColor="transparent"
+        translucent
+      />
+
+      {fontsLoaded ? <SignIn /> : <Loading />}
+
+    </NativeBaseProvider>
+  );
+}
